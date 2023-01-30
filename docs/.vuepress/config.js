c9d6432d8731abc1d7a902f1691fa07e92645839
liangzhengguang1 <liangzhengguang1@ad.zto.com>
2023-01-30 19:24:51 +0800

    WIP: update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,21 @@
 module.exports = {
-  base: './',
+  title: "lzg-UI",
+  base: "/lzg-ui/",
   themeConfig: {
-    sidebar: [
-      '/',
-      '/componentsDocs/card'
-    ]
-  }
-}
+    nav: [
+      {
+        text: "Home",
+        link: "/",
+      },
+      {
+        text: "Github",
+        link: "https://github.com/lzg2486/lzg-ui",
+      },
+      {
+        text: "Npm",
+        link: "https://www.npmjs.com/package/lzg-ui",
+      },
+    ],
+    sidebar: ["/", "/componentsDocs/card"],
+  },
+};
